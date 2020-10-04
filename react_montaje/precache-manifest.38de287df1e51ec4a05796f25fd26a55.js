9378300a4aacf1577126218e4d0c30e927068b60
Manolo <manolo@Mac-de-Manolo.local>
2020-10-04 18:33:58 +0800

    agregada la carpeta del montaje de React

---
+++ react_montaje/precache-manifest.38de287df1e51ec4a05796f25fd26a55.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658a86d187c203a5f034b184e6124ad4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80a713c9d36afb0cec7b",
+    "url": "/static/css/main.cfb873bf.chunk.css"
+  },
+  {
+    "revision": "405113aba3ef948f625e",
+    "url": "/static/js/2.21298b47.chunk.js"
+  },
+  {
+    "revision": "80a713c9d36afb0cec7b",
+    "url": "/static/js/main.a4752d35.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f0cd4c17d951a162c6d81e59367bb661",
+    "url": "/static/media/Heebo-Bold.f0cd4c17.ttf"
+  },
+  {
+    "revision": "a947b2571d8f5a7e2048399f947c2367",
+    "url": "/static/media/Heebo-Light.a947b257.ttf"
+  },
+  {
+    "revision": "0fc4376f729c856df411aaca50218c1e",
+    "url": "/static/media/OleoScript-Bold.0fc4376f.ttf"
+  },
+  {
+    "revision": "02670ba88de37bb89e6aa5ea084d81af",
+    "url": "/static/media/factura01.02670ba8.jpg"
+  },
+  {
+    "revision": "b0114b7d14532b62078154ffa6b2abd7",
+    "url": "/static/media/login-cabecera.b0114b7d.jpg"
+  }
+]);
