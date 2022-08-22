ad4187e83d18fc48e52d4c9a21204091e7435ef9
Henry Corredor <feijizoule@126.com>
2022-08-22 11:13:46 +0800

    keep going with implementation of sequelize

--- db/config.js
+++ db/config.js
@@ -2,21 +2,23 @@ const config = require('../config/')
 
 const USER = encodeURIComponent(config.dbUser)
 const PASSWORD = encodeURIComponent(config.dbPassword)
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbUrl}:${config.dbPort}/${config.dbName}`
+
 
 // development: {
-// 	username: config.dbUser,
-// 	password: config.dbPassword,
-// 	database: config.dbName,
-// 	host: config.dbHost,
-// 	port: config.dbPort,
-// 	dialect: "mysql"
+// 	uri: URI,
+// 	dialect: 'mysql'
 // }
+//console.log(URI)
 
 module.exports = {
 	development: {
-		uri: URI,
-		dialect: 'mysql'
+		username: config.dbUser,
+		password: config.dbPassword,
+		database: config.dbName,
+		host: config.dbUrl,
+		port: config.dbPort,
+		dialect: "mysql"
 	},
 	production: {
 		uri: URI,
